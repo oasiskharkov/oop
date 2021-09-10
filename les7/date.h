7c6b74e76242a8a2413111f4c8000f3a1504594f
oasiskharkov <kasystems@yandex.ru>
2021-09-10 18:14:59 +0300

    Add two tasks for lesson 7

---
+++ les7/date.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <iostream>
+
+class Date
+{
+public:
+   Date(int day, int month, int year) :
+      m_day{day},
+      m_month{month},
+      m_year{year}
+   {}
+   int day() const;
+   int month() const;
+   int year() const;
+   std::string to_string() const;
+   friend std::ostream& operator << (std::ostream& out, const Date& d);
+private:
+   int m_day;
+   int m_month;
+   int m_year;
+};
+
+inline int Date::day() const
+{
+   return m_day;
+}
+
+inline int Date::month() const
+{
+   return m_month;
+}
+
+inline int Date::year() const
+{
+   return m_year;
+}
+
+inline std::string Date::to_string() const
+{
+   char buf[11];
+   sprintf_s(buf, "%04d.%02d.%02d", m_year, m_month, m_day);
+   return buf;
+}
+
+std::ostream& operator << (std::ostream& out, const Date& d)
+{
+   out << d.m_day << '.' << d.m_month << '.' << d.m_year;
+   return out;
+}
+
---
+++ les7/les7.cpp
@@ -0,0 +1,84 @@
+﻿#include <memory>
+#include <iostream>
+
+#include "date.h"
+
+std::unique_ptr<Date> compare(const std::unique_ptr<Date>& d1, const std::unique_ptr<Date>& d2);
+void swap_uniques(std::unique_ptr<Date>& d1, std::unique_ptr<Date>& d2);
+
+int main()
+{
+   // ========== TASK #1 ==========
+   std::unique_ptr<Date> today = std::make_unique<Date>(10, 9, 2021);
+   std::cout << *today << std::endl;
+   std::cout << "Day: " << today->day() << std::endl;
+   std::cout << "Month: " << (*today).month() << std::endl;
+   std::cout << "Year: " << today.get()->year() << std::endl;
+   std::unique_ptr<Date> date;
+   today.swap(date);
+
+   if (today)
+   {
+      std::cout << *today << std::endl;
+   }
+   else
+   {
+      std::cout << "today is nullptr" << std::endl;
+   }
+
+   if (date)
+   {
+      std::cout << *date << std::endl;
+   }
+   else
+   {
+      std::cout << "date is nullptr" << std::endl;
+   }
+
+   today.reset(new Date(11, 9, 2021));
+   std::cout << *today << std::endl;
+
+   date.release();
+   if (!date)
+   {
+      std::cout << "Date released" << std::endl;
+   }
+
+   // ========== TASK #2 ==========
+   std::unique_ptr<Date> d1 = std::make_unique<Date>(10, 6, 2021);
+   std::unique_ptr<Date> d2(new Date(15, 6, 2021));
+
+   std::unique_ptr<Date> greater = compare(d1, d2);
+   std::cout << *greater << std::endl;
+
+   swap_uniques(d1, d2);
+   std::cout << *d1 << std::endl;
+   std::cout << *d2 << std::endl;
+
+   return EXIT_SUCCESS;
+}
+
+std::unique_ptr<Date> compare(const std::unique_ptr<Date>& d1, const std::unique_ptr<Date>& d2)
+{
+   int result = strcmp(d1->to_string().c_str(), d2->to_string().c_str());
+   if (result >= 0)
+   {
+      return std::make_unique<Date>(d1->day(), d1->month(), d1->year());
+   }
+   return std::make_unique<Date>(d2->day(), d2->month(), d2->year());
+}
+
+void swap_uniques(std::unique_ptr<Date>& d1, std::unique_ptr<Date>& d2)
+{
+   std::unique_ptr<Date> temp(std::move(d1));  // 1 way
+   d1 = std::move(d2);
+   d2 = std::move(temp);
+
+   /*Date* temp = d1.get();                    // 2 way 
+   d1.release();
+   d1.reset(d2.get());
+   d2.release();
+   d2.reset(temp);*/
+
+   /*std::swap(d1, d2);*/                      // 3 way
+}
